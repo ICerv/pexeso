faf8b42f44a77ff3056baa4e206741cbd8668f62
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-07 23:53:51 +0200

    Task completed

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-console.log('Funguju!')
+const cards = document.querySelectorAll('.karticka');
 
-const karticky = document.querySelectorAll('.karticka')
+// cards[0].classList.remove('otocena');
 
-karticky[0].classList.remove('otocena')
+let allCards = [];
+let turnCard = (karticka) => {
+  karticka.classList.toggle('otocena');
+};
+
+cards.forEach((karticka) => {
+  karticka.addEventListener('click', () => {
+    if (allCards.length < 2) {
+      turnCard(karticka);
+      allCards.push(karticka);
+
+      if (allCards.length === 2) {
+        const firstCard = allCards[0];
+        const secondCard = allCards[1];
+
+        if (
+          firstCard.querySelector('img').src ===
+          secondCard.querySelector('img').src
+        ) {
+          firstCard.disabled = true;
+          secondCard.disabled = true;
+          allCards = [];
+        } else {
+          setTimeout(() => {
+            turnCard(firstCard);
+            turnCard(secondCard);
+            allCards = [];
+          }, 1000);
+        }
+      }
+    }
+  });
+});
